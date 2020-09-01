ab01a7663df3ddb6f1e84630760fc8bc00bdec64
Jon Harper <jon.harper87@gmail.com>
2020-09-01 10:56:18 +0200

    Remove study references and add an XXX marker for places to be modified
    Signed-off-by: Jon Harper <jon.harper87@gmail.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware('http://localhost:5001/api/study-server', {
-        pathRewrite: { '^/api/api-server/': '/' }
+    createProxyMiddleware('http://localhost:8080/api/XXX-server', {
+        pathRewrite: { '^/api/XXX-server/': '/' }
     })
+  );
+  app.use(
+      createProxyMiddleware('http://localhost:8080/ws/XXX-server', {
+          pathRewrite: { '^/ws/XXX-server/': '/' },
+          ws: true,
+      })
+  );
+  app.use(
+      createProxyMiddleware('http://localhost:9000/api/gateway', {
+          pathRewrite: { '^/api/gateway/': '/' },
+      })
+  );
+  app.use(
+      createProxyMiddleware('http://localhost:9000/ws/gateway', {
+          pathRewrite: { '^/ws/gateway/': '/' },
+          ws: true,
+      })
   );
 };
