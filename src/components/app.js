ffe204b30b825c4220940813aa0c8a3863894cc1
HARPER Jon <jon.harper87@gmail.com>
2022-08-31 08:15:45 +0200

    prettier

--- src/components/app.js
+++ src/components/app.js
@@ -224,28 +224,42 @@ const App = () => {
             <AppTopBar user={user} userManager={userManager} />
             {user !== null ? (
                 <Routes>
-                    <Route path="/" element={
-                        <Box mt={20}>
-                            <Typography
-                                variant="h3"
-                                color="textPrimary"
-                                align="center"
-                            >
-                                Connected
-                            </Typography>
-                        </Box>
-                    }/>
-                    <Route path="/sign-in-callback" element={
-                        <Navigate replace to={getPreLoginPath() || '/'} />
-                    }/>
-                    <Route path="/logout-callback" element={
-                        <h1>Error: logout failed; you are still logged in.</h1>
-                    }/>
-                    <Route path="*" element={
-                        <h1>
-                            <FormattedMessage id="PageNotFound" />
-                        </h1>
-                    }/>
+                    <Route
+                        path="/"
+                        element={
+                            <Box mt={20}>
+                                <Typography
+                                    variant="h3"
+                                    color="textPrimary"
+                                    align="center"
+                                >
+                                    Connected
+                                </Typography>
+                            </Box>
+                        }
+                    />
+                    <Route
+                        path="/sign-in-callback"
+                        element={
+                            <Navigate replace to={getPreLoginPath() || '/'} />
+                        }
+                    />
+                    <Route
+                        path="/logout-callback"
+                        element={
+                            <h1>
+                                Error: logout failed; you are still logged in.
+                            </h1>
+                        }
+                    />
+                    <Route
+                        path="*"
+                        element={
+                            <h1>
+                                <FormattedMessage id="PageNotFound" />
+                            </h1>
+                        }
+                    />
                 </Routes>
             ) : (
                 <AuthenticationRouter
