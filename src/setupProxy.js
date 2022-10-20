290a75de384f94e2b6063c832f6cec36d67db3bf
sBouzols <sylvain.bouzols@gmail.com>
2022-10-20 10:34:04 +0200

    chore(): setupProxy only allowing gateway
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
-        createProxyMiddleware('http://localhost:8080/api/XXX-server', {
-            pathRewrite: { '^/api/XXX-server/': '/' },
-        })
-    );
-    app.use(
-        createProxyMiddleware('http://localhost:8080/ws/XXX-server', {
-            pathRewrite: { '^/ws/XXX-server/': '/' },
-            ws: true,
-        })
-    );
-    app.use(
         createProxyMiddleware('http://localhost:9000/api/gateway', {
             pathRewrite: { '^/api/gateway/': '/' },
         })
