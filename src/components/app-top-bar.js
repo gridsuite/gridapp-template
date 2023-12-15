0f03cf448b9c8a9f340d642325d0dc7141bb825b
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2023-12-15 09:04:50 +0100

    Update "about" window
    Update commons-ui and use new API

--- src/components/app-top-bar.js
+++ src/components/app-top-bar.js
@@ -59,43 +59,10 @@ const AppTopBar = ({ user, userManager }) => {
                 onLogoClick={() => navigate.replace('/')}
                 user={user}
                 appsAndUrls={appsAndUrls}
-                getGlobalVersion={(setGlobalVersion) =>
-                    fetchVersion()
-                        .then((res) => setGlobalVersion(res.deployVersion))
-                        .catch((reason) => {
-                            console.error(
-                                'Error while fetching the version : ' + reason
-                            );
-                            setGlobalVersion(null);
-                        })
+                globalVersionPromise={() =>
+                    fetchVersion().then((res) => res?.deployVersion)
                 }
-                getAdditionalModules={(setServers) =>
-                    getServersInfos(user?.id_token)
-                        .then((res) =>
-                            setServers(
-                                Object.entries(res).map(([name, infos]) => ({
-                                    name:
-                                        infos?.build?.name ||
-                                        infos?.build?.artifact ||
-                                        name,
-                                    type: 'server',
-                                    version: infos?.build?.version,
-                                    gitTag:
-                                        infos?.git?.tags ||
-                                        infos?.git?.commit?.id[
-                                            'describe-short'
-                                        ],
-                                }))
-                            )
-                        )
-                        .catch((reason) => {
-                            console.error(
-                                'Error while fetching the servers infos : ' +
-                                    reason
-                            );
-                            setServers(null);
-                        })
-                }
+                additionalModulesPromise={getServersInfos}
                 onThemeClick={handleChangeTheme}
                 theme={themeLocal}
                 onLanguageClick={handleChangeLanguage}
