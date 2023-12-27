558fd23d82b43a04aa9f52d72a7363350068b17f
Tristan <135599584+Tristan-WorkGH@users.noreply.github.com>
2023-12-27 11:33:54 +0100

    Update "about" window (#56)
    Update commons-ui and use new API

--- src/rest/study.js
+++ src/rest/study.js
@@ -5,14 +5,16 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 import { backendFetchJson } from '../utils/rest-api';
+
 const API_URL =
     '/api/' +
     (process.env.REACT_APP_USE_AUTHENTICATION === 'true'
         ? process.env.REACT_APP_API_GATEWAY + '/study/v1'
         : process.env.REACT_APP_SRV_STUDY_URI + '/v1');
+
 export function getServersInfos(token) {
     return backendFetchJson(
-        `${API_URL}/servers/infos`,
+        `${API_URL}/servers/about`,
         {
             headers: {
                 Accept: 'application/json',
@@ -21,5 +23,8 @@ export function getServersInfos(token) {
             cache: 'default',
         },
         token
-    );
+    ).catch((reason) => {
+        console.error('Error while fetching the servers infos : ' + reason);
+        return reason;
+    });
 }
