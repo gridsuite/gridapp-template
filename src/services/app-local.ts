b42d0dc27945c18afed100152f77c064e33fdaaa
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-08-12 12:43:36 +0200

    update commons-ui

--- src/services/app-local.ts
+++ src/services/app-local.ts
@@ -13,7 +13,7 @@ export default class AppLocalSvc extends AppLocalComSvc {
         super();
     }
 
-    public async fetchEnv(): Promise<EnvJson> {
+    public async fetchEnv() {
         return (await super.fetchEnv()) as EnvJson;
     }
 }
