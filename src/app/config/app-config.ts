350466a6373d333909023d2a5a3a63f96261fc73
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-04-14 14:34:44 +0200

    Fix merge

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -5,11 +5,4 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-<<<<<<<< HEAD:src/app/config/app-config.ts
 export const APP_NAME = 'XXX';
-========
-import { TextEncoder, TextDecoder } from 'util';
-
-// fix for ReferenceError: TextDecoder / TextEncoder is not defined
-Object.assign(global, { TextDecoder, TextEncoder });
->>>>>>>> origin/main:jest.setup.ts
