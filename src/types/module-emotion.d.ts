2a05fb3ca8150484ea5c0a83c95063b306ef14a2
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-04-14 16:38:26 +0200

    PR remark
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/types/module-emotion.d.ts
+++ src/types/module-emotion.d.ts
@@ -11,7 +11,6 @@
 import { Theme as EmotionTheme } from '@emotion/react';
 import { Theme as MaterialUITheme } from '@mui/material';
 
-// import '@emotion/react';
 declare module '@emotion/react' {
     // Re-declare the emotion theme to have the properties of the MaterialUiTheme
     export interface Theme extends EmotionTheme, MaterialUITheme {}
