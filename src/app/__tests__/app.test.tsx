ab66177301894bf8fafbbd12c52723759ff80c62
Caroline Jeandat <caroline.jeandat@rte-france.com>
2026-08-10 11:03:30 +0200

    fix app test

--- src/app/__tests__/app.test.tsx
+++ src/app/__tests__/app.test.tsx
@@ -12,12 +12,27 @@ import { BrowserRouter } from 'react-router';
 import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
 import { it, expect, vi } from 'vitest';
 import { SnackbarProvider } from '@gridsuite/commons-ui';
+import { http, HttpResponse } from 'msw';
 import App from '../App';
 import { store } from '../store/store';
+import { server } from '../../test-utils/msw/server';
 
 vi.mock('uuid', () => ({ v4: () => '00000000-0000-0000-0000-000000000000' }));
 
 it('renders', async () => {
+    server.use(
+        http.get('*/env.json', () =>
+            HttpResponse.json({
+                appsMetadataServerUrl: 'http://localhost:8070',
+            })
+        ),
+        http.get('http://localhost:8070/version.json', () =>
+            HttpResponse.json({
+                deployVersion: 'test-version',
+            })
+        )
+    );
+
     render(
         <IntlProvider locale="en">
             <BrowserRouter>
@@ -35,5 +50,9 @@ it('renders', async () => {
         </IntlProvider>
     );
 
-    expect(await screen.findByText(/XXX/i)).toBeInTheDocument();
+    const res = await screen.findByText((_, element) => {
+        return element?.textContent === 'XXX';
+    });
+
+    expect(res).toBeInTheDocument();
 });
